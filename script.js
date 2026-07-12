aeba28f102fb3d5ad49912f986a417d3f15e1951
muhyusufpurnomo-ship-it <muhyusuf102131@gmail.com>
2026-07-12 12:09:50 +0700

    Responsive mobile improvement and scroll reveal animation

--- script.js
+++ script.js
@@ -1560,3 +1560,33 @@ console.log(
 "Elegant Royal Javanese Wedding Loaded Successfully"
 );
 });
+
+/* =======================================================
+   SCROLL REVEAL
+======================================================= */
+
+const revealElements = document.querySelectorAll(".reveal");
+
+const revealObserver = new IntersectionObserver((entries) => {
+
+    entries.forEach(entry => {
+
+        if(entry.isIntersecting){
+
+            entry.target.classList.add("show");
+
+        }
+
+    });
+
+},{
+
+    threshold:0.2
+
+});
+
+revealElements.forEach(item=>{
+
+    revealObserver.observe(item);
+
+});
